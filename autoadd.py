4de9e6b0e80f418f0715494396ed110af34474cc
srest2021 <68917129+srest2021@users.noreply.github.com>
2020-08-05 15:48:46 -0400

    Second upload

--- autoadd.py
+++ autoadd.py
@@ -3,15 +3,40 @@
 from astropy.io import ascii
 import argparse
 from astrotable import astrotableclass
+from tools import yamlcfgclass
 import pandas as pd
 from lxml import html
 import requests
-import sys
+import sys,os
 
 class autoaddclass(astrotableclass):
 	def __init__(self):
 		astrotableclass.__init__(self)
-		self.snlist = ascii.read('snlist.txt')
+
+		self.cfg = yamlcfgclass()
+		self.debug = False
+		self.outrootdir = None
+		self.snlist = None
+
+	def define_options(self):
+		if 'ATLASLC_SOURCEDIR' in os.environ and os.environ['ATLASLC_SOURCEDIR'] != '':
+			outrootdir = os.environ['ATLASLC_DATA']
+			cfgfile = '%s/precursor.cfg' % os.environ['ATLASLC_SOURCEDIR']
+		else:
+			cfgfile = None
+			outrootdir = None
+		return(cfgfile)
+
+	def setoutdir(self,outrootdir=None):
+		if outrootdir is None:
+			basedir = self.cfg.params['output']['outrootdir']
+		else:
+			basedir = outrootdir
+		self.outrootdir = basedir
+
+	def loadcfgfiles(self, *pargs, **kwargs):
+		result=self.cfg.loadcfgfiles(*pargs, **kwargs)
+		return(result)
 
 	def getradec(self, tnsname):
 		obj_name=tnsname
@@ -37,11 +62,6 @@ class autoaddclass(astrotableclass):
 		self.snlist.add_row({'atlasdesignation':'x','otherdesignation':'x','ra':ra,'dec':dec,'spectraltype':'x','earliestmjd':'x','tnsname':tnsname})
 		print('snlist.txt NEW: \n',self.snlist)
 
-	def savesnlist(self):
-		ascii.write([self.snlist['atlasdesignation'], self.snlist['otherdesignation'], self.snlist['ra'], self.snlist['dec'], self.snlist['spectraltype'], self.snlist['earliestmjd'], self.snlist['tnsname']], 
-			'snlist.txt', names=['atlasdesignation', 'otherdesignation', 'ra', 'dec', 'spectraltype', 'earliestmjd', 'tnsname'], 
-			format='fixed_width_two_line', overwrite=True)
-
 	def getcmd(self, tnsname):
 		print('Your command is: download_lc_loop_sofia.py %s -v -o -s --forcedphot_offset True --plot True --averagelc True --MJDbinsize 1 -l 70' % tnsname)
 
@@ -54,6 +74,12 @@ if __name__ == '__main__':
 	parser.add_argument('--dec', help="Dec position", default=None, type=str)
 	args = parser.parse_args()
 
+	cfgfile=autoadd.define_options()
+	autoadd.loadcfgfiles(cfgfile)
+	autoadd.setoutdir()
+	snlistfilename = autoadd.cfg.params['snlistfilename']
+	autoadd.snlist = ascii.read(snlistfilename)
+
 	if not args.ra:
 		print(args.tnsname)
 		ra, dec = autoadd.getradec(args.tnsname)
@@ -62,5 +88,9 @@ if __name__ == '__main__':
 		dec=args.dec
 
 	autoadd.addrow2snlist(tnsname=args.tnsname,ra=ra,dec=dec)
-	autoadd.savesnlist()
+
+	ascii.write([autoadd.snlist['atlasdesignation'], autoadd.snlist['otherdesignation'], autoadd.snlist['ra'], autoadd.snlist['dec'], autoadd.snlist['spectraltype'], autoadd.snlist['earliestmjd'], autoadd.snlist['tnsname']], 
+				snlistfilename, names=['atlasdesignation', 'otherdesignation', 'ra', 'dec', 'spectraltype', 'earliestmjd', 'tnsname'], 
+				format='fixed_width_two_line', overwrite=True)
+
 	autoadd.getcmd(tnsname=args.tnsname)
